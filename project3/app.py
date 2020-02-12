bc20ef8939c6da62a6e8c86773337faf60e8341b
Disaisoft <disaisoft@gmail.com>
2020-02-12 10:18:32 -0500

    project3 fecha y hora

--- project3/app.py
+++ project3/app.py
@@ -6,6 +6,7 @@ d = date.today()
 d.strftime("/%d/%m/%y")
 print(d)
 
-# obtener la fecha con el dia y la hora
+# obtener la fecha con el dia, mes, ano y la hora
 dt=datetime.now()
-dt.strftime("%A %d/%m/%y %H:%M:%S")
+dt.strftime("%A %d/%B/%y %H:%M:%S")
+print(dt)
