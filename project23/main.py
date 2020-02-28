928117419481362a21ec762e41cf41229faf90f0
Disaisoft <disaisoft@gmail.com>
2020-02-28 16:12:47 -0500

    ejercicio 22, incompleto

---
+++ project23/main.py
@@ -0,0 +1,15 @@
+import requests
+_ENDPOINT = "https://api.binance.com"
+
+def _url(api):
+    return _ENDPOINT+api 
+
+def get_price(cripto):
+    return requests.get(_url("/api/v3/ticker/price?symbol="+cripto))
+
+def esmoneda(cripto):
+    criptos = ["BTC","BCC","LTC","ETH","ETC","XRP"]
+    return cripto in criptos
+
+def esnumero(numero):
+    return numero.replace('.','',1).isdigit()
