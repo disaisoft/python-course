7a4e067acf1637659d2b86c97386c005e7f3486a
Disaisoft <disaisoft@gmail.com>
2020-02-28 09:14:15 -0500

    ejercicio 21

--- project21/main.py
+++ project21/main.py
@@ -5,15 +5,15 @@ y precios en USD, haciendo uso de arreglos. Posteriormente, imprima los datos
 de cada moneda luego de ser ingresadas por el usuario.
 """
 
-str cripto [5]
-float cant[5], cotiz[5]
-int i=0
+cripto [5]
+cant[5], cotiz[5]
+i=0
 
 while i<5 do
     cripto[i]=read("Ingrese el nombre de la moneda: ")
     cant[i]=float(read("Ingrese la cantidad de "+cripto[i]+":")
-cotiz[i]=float(read("Ingrese la cotización en USD de "+cripto[i]+":")
-i=i+1
+    cotiz[i]=float(read("Ingrese la cotización en USD de "+cripto[i]+":")
+    i=i+1
 endwhile
 i=0
 while i<5 do
