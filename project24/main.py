75bd92f61964c73ba74c1b188972174eb9bf828f
Disaisoft <disaisoft@gmail.com>
2020-03-02 11:50:27 -0500

    ejercicio 24, verificar nombre de la criptomoneda si existe o no!

---
+++ project24/main.py
@@ -0,0 +1,25 @@
+"""
+es una coleccion no ordenada de valores unicos
+los connjuntos pueden tener valores de cualquier tipo!
+cuando es dos o mas conjuntos se realizan: union, intersenccion y diferencia de conjunto
+"""
+
+#para crear un conjunto EJEMPLO:
+# OPERACIONES DE CONJUNTOS - s.add(e) = agrega el elemento e en s.!!! 
+# s.clear() = elimina todos los elemtos del conjunto s.!!! s.copy() = retorna una copia del conjunto s.
+
+"""
+conjunto = set()
+
+conj_a = {1}
+conj_b = {1, 2, 3}
+conj_c = set(['a','b',42])
+"""
+
+"""
+Ejercicio: En esta ocasión te solicitamos que escribas un programa en Python que verifique 
+si el nombre de una criptomoneda, ingresada por el usuario, existe en coinmarketcap.com. 
+Para ello recuerda utilizar el módulo requests, usando la API https://api.coinmarketcap.com/v2/listings/. 
+Esta API retorna un json con una lista de datos de las criptomonedas, entre estos datos se encuentra 
+el symbol de la criptomoneda que corresponde al nombre código de la moneda.
+"""
