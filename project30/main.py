26aaf6da672efa845977a45e58ffad32ecfff9dc
Disaisoft <disaisoft@gmail.com>
2020-03-04 17:16:41 -0500

    ejercicio 30, iniciando POO

---
+++ project30/main.py
@@ -0,0 +1,20 @@
+
+"""
+Programacion orientad a objetos!
+- conceptos que lo soportan!
+1 - astraccion de datos: proceso mediante el cual somos capaces de escoger la implementacion
+de un objeto que contenga o modele las propiedades en el problema.
+2 - encapsulamiento: ocultar la representacion y el estado de las propiedades de un objeto! solamente 
+se puede modificar a traves de las operaciones definidas en una clase
+3 - herencia: 
+4 - polimorfismo
+
+CLASE: conjunto de objetos que comparten una estructura, comportamiento y semantica comun.
+OBJETO: es una instancia o valor  correspondiente a una clase! representacion astrabta de un concepto
+"""
+
+
+#persona = "jhon", "angela"
+#print(persona)
+#--------------------------------
+
